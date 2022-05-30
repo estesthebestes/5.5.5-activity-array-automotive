3c35d216b9c6fccdbf0898f22f4af28b66a8d1f7
estesthebestes <esteshunter22@gmail.com>
2022-05-30 13:27:55 -0500

    I have some type of structural issue in my code that is showing errors in the methods I have made but I have not been able to find it

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,11 +1,11 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+const VehicleModule = require("./vehicle").Vehicle;
 
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
 
-class car extends VehicleModule {
+class Car extends VehicleModule {
     constructor (make, model, year, color, mileage) {
         super(make, model, year, color, mileage);
         this.maximumPassengers = 5;
@@ -20,9 +20,27 @@ class car extends VehicleModule {
     }
 }
 
-serviceCheck(){
-    if (this.mileage > 10000) {
-            this.scheduleService = true;
-            return this.scheduleService;
-        } 
+serviceNeeded(); {
+    if (this.mileage > 30000) {            
+        this.scheduleService = true
+        this.scheduleService;                       
+    }
+}
+
+loadPassenger(passenger, maximumPassengers) { 
+    if (this.passenger < this.maximumPassengers) {
+        console.log('You have room for more passengers.');
+    } else {
+        console.log('You are at maximum capacity.');
+    }
+}
+
+start() {
+    if (this.fuel > 0){
+        return this.started = true;
+        console.log("engine started...!!!");
+    } else {
+        return this.started = false;
+        console.log("engine cannot start...");
+    }
 }
