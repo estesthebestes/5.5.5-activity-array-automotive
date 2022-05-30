0d6f4ac3e2fbdf403fc4f1fa8c0b29a4bb7307b6
estesthebestes <esteshunter22@gmail.com>
2022-05-30 12:45:14 -0500

    added first changes, trying to set up the structure of the solution

--- mecurySedan.js
+++ mecurySedan.js
@@ -4,3 +4,7 @@ const VehicleModule = require("./vehicleBaseClass")
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
+
+class car extends VehicleModule {
+    constructor 
+}
