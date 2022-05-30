de2c3a725f97e0cc70736f67090ee7a82e042610
estesthebestes <esteshunter22@gmail.com>
2022-05-30 13:05:27 -0500

    added new fields, trying to fix bug in the logic

--- mecurySedan.js
+++ mecurySedan.js
@@ -6,5 +6,23 @@ let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage")
 console.log(v.make)
 
 class car extends VehicleModule {
-    constructor 
+    constructor (make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maximumPassengers = 5;
+        this.transmission = "automatic";
+        this.passenger = 0;
+        this.numberOfWheels = 4; 
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+        this.fuelMileageHwy = 28;
+        this.fuelMileageCity = 20;
+    }
+}
+
+serviceCheck(){
+    if (this.mileage > 10000) {
+            this.scheduleService = true;
+            return this.scheduleService;
+        } 
 }
